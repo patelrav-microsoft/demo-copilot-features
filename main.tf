3dc19d6d7d460adccdfef43c35e9b7774b937118
Ravi Patel <patelrav@microsoft.com>
2026-04-02 07:03:20 -0500

    new commit

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix for resource names"
+  default     = "demo"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region for resources"
+  default     = "canadacentral"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to resources"
+  default = {
+    environment = "demo"
+    owner       = "team"
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg-fordemo"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "${var.prefix}-asp-fordemo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+
+  tags = var.tags
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "app_service_plan_name" {
+  value = azurerm_app_service_plan.asp.name
+}
